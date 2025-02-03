bf5ca33c73ced202e0f0511958e4b345fc637064
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-02-03 07:04:08 +0600

    Update 1.10.c

--- Chapter 1 - Introduction to Computing./1.10.c
+++ Chapter 1 - Introduction to Computing./1.10.c
@@ -3,12 +3,12 @@ digits present in a number.
 
 1.10 Algorithm and Flowchart to Count Number of Digits in a Number
 Algorithm:
-Start
-Input N.
-Initialize count = 0.
-While N > 0:
-N = N // 10 (Remove last digit)
-Increment count.
-Display count.
-Stop.
+1.Start
+2.Input N.
+3.Initialize count = 0.
+4.While N > 0:
+5.N = N // 10 (Remove last digit)
+6.Increment count.
+7.Display count.
+8.Stop.
 */
