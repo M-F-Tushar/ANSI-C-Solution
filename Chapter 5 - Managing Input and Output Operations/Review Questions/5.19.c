930e089b8641bdadbc67552c5172957257ed5592
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-02-23 18:48:27 +0600

    Add files via upload

---
+++ Chapter 5 - Managing Input and Output Operations/Review Questions/5.19.c
@@ -0,0 +1,12 @@
+/*
+How are the unrecognized characters within the control string are
+interpreted in
+(a) scanf function; and
+(b) printf unction?
+*/
+(a) In scanf():
+
+Causes input mismatch.
+(b) In printf():
+
+Printed as-is.
