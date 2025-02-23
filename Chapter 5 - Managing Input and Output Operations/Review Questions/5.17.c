590a5d842e250eb186bb431f6eb21529ef304a14
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-02-23 18:47:38 +0600

    Add files via upload

---
+++ Chapter 5 - Managing Input and Output Operations/Review Questions/5.17.c
@@ -0,0 +1,11 @@
+/*
+How does a control string in a printf ( ) function differ from the
+control string in a scanf ( ) function?
+*/
+printf() displays output.
+scanf() reads input.
+
+Example:
+
+printf("%d", x);  // Prints an integer
+scanf("%d", &x);  // Reads an integer
