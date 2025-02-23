bac127f3c9c186309c78777edb800cc95f51c31d
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-02-23 18:46:29 +0600

    Add files via upload

---
+++ Chapter 5 - Managing Input and Output Operations/Review Questions/5.14.c
@@ -0,0 +1,12 @@
+/*
+5.14
+What happens when an input data item contains
+(a) more characters than the specified field width and
+(b) fewer characters than the specified field width?
+*/
+(a) More characters:
+
+Extra characters stay in the buffer.
+(b) Fewer characters:
+
+Program waits for more input.
