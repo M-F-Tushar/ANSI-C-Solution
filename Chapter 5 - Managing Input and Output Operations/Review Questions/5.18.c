4dbe44141fdb7cb3bc587ca4c92ef0bb6b507099
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-02-23 18:48:04 +0600

    Add files via upload

---
+++ Chapter 5 - Managing Input and Output Operations/Review Questions/5.18.c
@@ -0,0 +1,11 @@
+/*
+What happens if an output data item contains
+(a) more characters than the specified field width and
+(b) fewer characters than the specified field width?
+*/
+(a) More characters:
+
+No issue, just prints all.
+(b) Fewer characters:
+
+Right-padded with spaces.
