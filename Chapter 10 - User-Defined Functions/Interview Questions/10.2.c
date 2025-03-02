33277df16ed59c9b747056abe0e19565c3f395a5
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-02 09:04:02 +0600

    Add files via upload

---
+++ Chapter 10 - User-Defined Functions/Interview Questions/10.2.c
@@ -0,0 +1,6 @@
+/*
+What is the difference between formal arguments and actual
+arguments?
+Formal Arguments: Parameters in a function definition. (e.g., int sum(int a, int b))
+Actual Arguments: Values passed when calling the function. (e.g., sum(5, 10);)
+*/
