7b7052278d09ccbac0c3439a17af1ab3f2cc795f
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-02 08:56:32 +0600

    Add files via upload

---
+++ Chapter 10 - User-Defined Functions/Review Questions/10.3.c
@@ -0,0 +1,6 @@
+10.3 The main is a user-defined function. How does it differ from other user-defined functions?
+✅ Answer:
+
+main() is the entry point of the program, whereas other functions are called by main().
+main() cannot be called like a normal function in most cases.
+The operating system calls main().
