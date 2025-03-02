2dece3edbe8f5bac3d0930df910321ee7c5a6585
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-02 08:58:23 +0600

    Add files via upload

---
+++ Chapter 10 - User-Defined Functions/Review Questions/10.10.c
@@ -0,0 +1,16 @@
+A function to divide two floating point numbers is as follows:
+c
+Copy
+Edit
+float divide (float x, float y) {
+    return (x / y);
+}
+Find the output of these function calls:
+Function Call	Output
+divide(10, 2)	5.0
+divide(9, 2)	4.5
+divide(4.5, 1.5)	3.0
+divide(2.0, 3.0)	0.66667
+📌 Reason:
+
+Since x and y are floats, the result is a floating-point division.
