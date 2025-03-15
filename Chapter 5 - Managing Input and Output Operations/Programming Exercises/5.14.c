a42f985a5f7434c87fb2277873499fde254e428b
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-15 10:36:34 +0600

    Add files via upload

---
+++ Chapter 5 - Managing Input and Output Operations/Programming Exercises/5.14.c
@@ -0,0 +1,20 @@
+// Programming Exercise 5.14
+// Display a given pattern
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
