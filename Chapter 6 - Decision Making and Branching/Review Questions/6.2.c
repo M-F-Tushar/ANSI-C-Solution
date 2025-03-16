d1ca541e96e99d45c1d8ea0a10ef7b8876a1f80f
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-16 19:43:01 +0600

    Add files via upload

---
+++ Chapter 6 - Decision Making and Branching/Review Questions/6.2.c
@@ -0,0 +1,14 @@
+6.2 Fill in the blanks in the following statements:
+(a) The AND (&&) operator is true only when both the operands are true.
+
+(b) Multiway selection can be accomplished using an else if statement or the switch statement.
+
+(c) The break statement when executed in a switch statement causes an immediate exit from the structure.
+
+(d) The expression !(x != y) can be replaced by the expression x == y.
+
+(e) The ternary conditional expression using the operator ? : could be easily coded using if-else.
+
+(f) The if statement is used to build two-way decision-making statements that return one of the two values based on the result of the expression.
+
+(g) The goto statement is used to branch unconditionally from one point to another in a C program.
