9bd71550b01ebcef5aa76e70e9a45fb546f935bd
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-16 19:47:01 +0600

    Add files via upload

---
+++ Chapter 6 - Decision Making and Branching/Review Questions/6.16.c
@@ -0,0 +1,25 @@
+
+Problem 6.16
+Code:
+c
+char ch = 'a';
+switch (ch) {
+    case 'a':
+        printf("A");
+        break;
+    case 'b':
+        printf("B");
+        break;
+    default:
+        printf("C");
+}
+Step-by-Step Execution:
+Character
+𝑐h=′𝑎′.
+
+The switch statement evaluates 𝑐h:
+
+Case 'a' matches, so "A" is printed, and the break statement stops further execution.
+
+Output:
+A
