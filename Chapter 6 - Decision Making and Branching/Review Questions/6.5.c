bb5faba117d1ea97185618e2d94907365db83f88
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-16 19:43:35 +0600

    Add files via upload

---
+++ Chapter 6 - Decision Making and Branching/Review Questions/6.5.c
@@ -0,0 +1,13 @@
+6.5 Assuming x = 10, state whether the following logical expressions are true or false:
+(a) x == 10 && x > 10 && !x
+
+False (because x > 10 is false).
+(b) x == 10 || x > 10 && !x
+
+True (because x == 10 is true).
+(c) x == 10 && x > 10 || !x
+
+False (because x > 10 is false, and !x is also false).
+(d) x == 10 || x > 10 || !x
+
+True (because x == 10 is true).
