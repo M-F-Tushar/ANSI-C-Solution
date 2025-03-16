f04789938375976927e04f12f984a9ce9196ad8a
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-16 19:46:22 +0600

    Add files via upload

---
+++ Chapter 6 - Decision Making and Branching/Review Questions/6.14.c
@@ -0,0 +1,16 @@
+
+Problem 6.14
+Code:
+c
+int x = 10, y = 15;
+x = (x > y) ? (y + x) : (y - x);
+Step-by-Step Execution:
+Variable
+𝑥=10, =15.
+
+Evaluate the condition
+𝑥>𝑦: Since 10>15is false, the else part of the ternary operator executes:
+
+𝑥=𝑦−𝑥=1510=5.
+
+Value of 𝑥:5
