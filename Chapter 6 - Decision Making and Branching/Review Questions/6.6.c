749f12c59a692f791d27712613cdd4112caa3176
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-16 19:44:05 +0600

    Add files via upload

---
+++ Chapter 6 - Decision Making and Branching/Review Questions/6.6.c
@@ -0,0 +1,13 @@
+6.6 Find errors, if any, in the following switch-related statements:
+(a) switch (y);
+
+Error: The semicolon ; after switch(y) is incorrect. It should be followed by {} with cases inside.
+(b) case 10:
+
+Error: case should be inside a switch block. It is invalid when used alone.
+(c) switch (x + y)
+
+No Error: This is valid as long as there are cases defined inside the switch.
+(d) switch (x) {case 2: y = x + y; break;}
+
+No Error: This is a correct switch statement.
