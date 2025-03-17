bd2df58274eaeda44de0b1d7ed7accfd78903fc7
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-17 20:54:08 +0600

    Add files via upload

---
+++ Chapter 6 - Decision Making and Branching/Interview Questions/6.1.c
@@ -0,0 +1,2 @@
+Question 6.1: Till how many levels can nested blocks be created in C?
+There is no theoretical limit to the levels of nested blocks in C, as long as the program compiles successfully. However, practical constraints like code readability and memory stack size impose limits.
