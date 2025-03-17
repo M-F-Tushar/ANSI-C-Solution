b59706f58b129b016cb0a637811f7096497fc945
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-17 20:54:38 +0600

    Add files via upload

---
+++ Chapter 6 - Decision Making and Branching/Interview Questions/6.3.c
@@ -0,0 +1,2 @@
+Question 6.3: What is the purpose of the goto statement?
+The goto statement is used to transfer control to a labeled statement within the same function. It can simplify some loop-based or conditionally driven flow control, but its use is discouraged due to reduced readability and potential to create "spaghetti code."
