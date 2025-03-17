4af376589399736ec38ab9f4e3624c5a858f0d40
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-17 20:55:19 +0600

    Add files via upload

---
+++ Chapter 6 - Decision Making and Branching/Interview Questions/6.6.c
@@ -0,0 +1,6 @@
+Question 6.6: What types of values are permitted to be used with a switch statement?
+The switch statement allows:
+
+Integral types: int, char, enum values, or constants convertible to int.
+
+Floating-point or complex types are not permitted.
