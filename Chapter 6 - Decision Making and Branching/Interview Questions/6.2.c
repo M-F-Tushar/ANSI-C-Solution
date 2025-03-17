5e9165292a53ae5d265b5f0ae4e38d14904cfb1f
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-17 20:54:17 +0600

    Add files via upload

---
+++ Chapter 6 - Decision Making and Branching/Interview Questions/6.2.c
@@ -0,0 +1,3 @@
+
+Question 6.2: What happens if the conditional expression is missing in an if statement?
+If the conditional expression is missing, the if block will not compile, and the compiler will throw a syntax error.
