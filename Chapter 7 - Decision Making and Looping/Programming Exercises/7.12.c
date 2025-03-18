34434672c3820afc5d9dc963554d9c336f0a501f
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-18 19:15:26 +0600

    Add files via upload

---
+++ Chapter 7 - Decision Making and Looping/Programming Exercises/7.12.c
@@ -0,0 +1,68 @@
+7.12 Compute Useful Life of an Item using Depreciation Formula
+Formula:
+
+𝑃
+=
+𝐶
+(
+1
+−
+𝑑
+)
+𝑛
+P=C(1−d)
+n
+
+We need to find n (useful life in years) when P (scrap value) is given.
+
+Algorithm:
+
+Take inputs C, d, and P.
+Solve for n using logarithms:
+𝑛
+=
+log
+⁡
+(
+𝑃
+/
+𝐶
+)
+log
+⁡
+(
+1
+−
+𝑑
+)
+n=
+log(1−d)
+log(P/C)
+​
+
+Print n.
+C Program:
+
+c
+Copy code
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double C, d, P, n;
+
+    printf("Enter original cost (C): ");
+    scanf("%lf", &C);
+
+    printf("Enter annual depreciation rate (d): ");
+    scanf("%lf", &d);
+
+    printf("Enter scrap value (P): ");
+    scanf("%lf", &P);
+
+    n = log(P / C) / log(1 - d);
+
+    printf("The useful life in years: %lf\n", n);
+    return 0;
+}
+
