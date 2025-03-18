ee9c41825f93aafc06d084fad48c703d2085de59
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-18 19:15:39 +0600

    Add files via upload

---
+++ Chapter 7 - Decision Making and Looping/Programming Exercises/7.13.c
@@ -0,0 +1,24 @@
+7.13 Print a square of size 5 using character 'S'
+Pattern:
+
+nginx
+Copy code
+S S S S S
+S S S S S
+S S S S S
+S S S S S
+S S S S S
+
+#include <stdio.h>
+
+int main() {
+    int size = 5;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            printf("S ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
