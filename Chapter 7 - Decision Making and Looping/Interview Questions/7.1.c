08c3f7f5b547a936277f894ba7d995b05688811f
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-18 19:18:41 +0600

    Add files via upload

---
+++ Chapter 7 - Decision Making and Looping/Interview Questions/7.1.c
@@ -0,0 +1,7 @@
+7.1 What is the value of variable a after the following code is executed?
+
+int a;
+for (a = 0; a < 10; a++) { }
+The loop runs from a = 0 to a = 9, then increments a to 10 before exiting.
+Final value of a = 10.
+
