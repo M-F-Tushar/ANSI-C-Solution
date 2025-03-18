e6cc0be2f9edba3944f0c11bf42406fe77acb92f
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-18 19:19:48 +0600

    Add files via upload

---
+++ Chapter 7 - Decision Making and Looping/Interview Questions/7.2.c
@@ -0,0 +1,5 @@
+7.2 What is the final value of x after the following code executes?
+
+for (x = 9; x >= 0; x--) { }
+The loop runs from x = 9 to x = 0, then decrements x to -1 before exiting.
+Final value of x = -1.
