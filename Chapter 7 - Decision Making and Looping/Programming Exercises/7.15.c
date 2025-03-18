a56704ae1f284ff42b18b04280c9f5628a5f7bc2
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-18 19:16:06 +0600

    Add files via upload

---
+++ Chapter 7 - Decision Making and Looping/Programming Exercises/7.15.c
@@ -0,0 +1,34 @@
+
+7.15 Modify the square pattern with 'O' at the center
+Pattern:
+
+mathematica
+Copy code
+S S S S S
+S S S S S
+S S O S S
+S S S S S
+S S S S S
+Algorithm:
+
+Use two nested loops.
+If i == size/2 && j == size/2, print 'O', else print 'S'.
+C Program:
+
+c
+Copy code
+#include <stdio.h>
+
+int main() {
+    int size = 5;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (i == size / 2 && j == size / 2)
+                printf("O ");
+            else
+                printf("S ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
