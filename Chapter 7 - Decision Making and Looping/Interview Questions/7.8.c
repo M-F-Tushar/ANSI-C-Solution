2d975f6fbcf88c4306337f4ca9880469f55cb8b3
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-18 19:21:02 +0600

    Add files via upload

---
+++ Chapter 7 - Decision Making and Looping/Interview Questions/7.8.c
@@ -0,0 +1,4 @@
+7.8 Examples of pre-test and post-test loops
+Pre-test loop: while(condition) { ... } (condition checked first)
+Post-test loop: do { ... } while(condition); (condition checked later)
+
