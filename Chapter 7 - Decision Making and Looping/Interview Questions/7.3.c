332e0c137cd88a61cb79ed00df9e6e554edb45fa
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-18 19:19:58 +0600

    Add files via upload

---
+++ Chapter 7 - Decision Making and Looping/Interview Questions/7.3.c
@@ -0,0 +1,10 @@
+7.3 What is the minimum number of times a do-while loop executes?
+A do-while loop executes at least once, even if the condition is false initially.
+
+Example:
+
+
+do {
+    printf("Runs at least once!\n");
+} while (0);
+Answer: Minimum execution = 1 time.
