f383ade496171134ec78ee41e3f64a86019d002d
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-19 18:43:31 +0600

    Add files via upload

---
+++ Chapter 8 - Array/Review Questions/8.6.c
@@ -0,0 +1,8 @@
+8.6 What is a data structure? Why is an array called a data structure?
+👉 Data structure – A data structure is a way of organizing and storing data in a computer so that it can be accessed and modified efficiently.
+
+👉 Why is an array a data structure?
+
+An array is a data structure because it stores a collection of elements of the same type in contiguous memory locations.
+Arrays allow direct access to elements using an index and are useful for efficient data manipulation.
+
