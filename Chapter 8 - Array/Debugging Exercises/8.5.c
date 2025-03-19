5e0698077d8fdca77bc207e7b89d82af3917975a
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-19 18:46:19 +0600

    Add files via upload

---
+++ Chapter 8 - Array/Debugging Exercises/8.5.c
@@ -0,0 +1,17 @@
+8.5 Output of the following program:
+
+main()
+{
+   int m[] = {1,2,3,4,5};
+   int x, y = 0;
+
+   for (x = 0; x < 5; x++)
+      y = y + m[x];
+
+   printf("%d", y);
+}
+👉 Output:
+
+Sum of elements: 1 + 2 + 3 + 4 + 5 = 15
+✅ Output = 15
+
