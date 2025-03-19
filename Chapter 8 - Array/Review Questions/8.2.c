d5f70b8d9ad1146e1b0869df2075095f7037d032
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-19 18:42:41 +0600

    Add files via upload

---
+++ Chapter 8 - Array/Review Questions/8.2.c
@@ -0,0 +1,11 @@
+8.2 Fill in the blanks:
+(a) The variable used as a subscript in an array is popularly known as _______ variable.
+👉 Index – An array subscript is called an index.
+
+(b) An array that uses more than two subscripts is referred to as _______ array.
+👉 Multidimensional – An array with more than two dimensions is called a multidimensional array.
+
+(c) _______ is the process of arranging the elements of an array in order.
+👉 Sorting – Arranging elements of an array in order is called sorting.
+
+
