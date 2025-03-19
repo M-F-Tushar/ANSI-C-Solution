794f161d5eb03aecdbff7da2fe51af7e22621936
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-19 18:43:55 +0600

    Add files via upload

---
+++ Chapter 8 - Array/Review Questions/8.8.c
@@ -0,0 +1,8 @@
+
+8.8 What happens when an array with a specified size is assigned
+(a) with values fewer than the specified size;
+👉 Remaining values are initialized to zero (if the array is static) or garbage values (if the array is automatic).
+
+(b) with values more than the specified size.
+👉 It results in a compilation error or buffer overflow if it happens at runtime.
+
