9be8965f21b8f2e385855d656a9020043613d8ca
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-19 18:44:08 +0600

    Add files via upload

---
+++ Chapter 8 - Array/Review Questions/8.9.c
@@ -0,0 +1,11 @@
+
+8.9 Discuss how initial values can be assigned to a multidimensional array.
+👉 Example of assigning values to a multidimensional array:
+
+int matrix[3][2] = {
+    {1, 2},
+    {3, 4},
+    {5, 6}
+};
+👉 The compiler assigns values row by row.
+👉 Partial initialization fills remaining values with zeros.
