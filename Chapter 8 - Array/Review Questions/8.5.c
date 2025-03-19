71a8cbffaa797f90e4d36fd687adaee05ea2818d
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-19 18:43:23 +0600

    Add files via upload

---
+++ Chapter 8 - Array/Review Questions/8.5.c
@@ -0,0 +1,7 @@
+8.5 Which initialization statements are correct?
+Statement	                        Correct/Incorrect	Reason
+(a) char str1[4] = "GOOD";	        ❌ Incorrect	        String is too large for the array
+(b) char str2[2] = "C";	            ✅ Correct	        Fits the size
+(c) char str3[5] = "Moon";	        ✅ Correct	        Null terminator fits in size
+(d) char str4[] = {'S', 'U', 'N'};	✅ Correct	        Proper character array initialization
+(e) char str5[10] = "Sun";	        ✅ Correct	        Fits within size
