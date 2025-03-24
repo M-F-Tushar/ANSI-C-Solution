4bfbbf4508baaab2219de9128759168580316cf4
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-24 09:07:56 +0600

    Add files via upload

---
+++ Chapter 11 - Structures and Unions/Review Questions/11.16.c
@@ -0,0 +1,8 @@
+
+11.16 Important points when implementing bit-fields in structures
+Bit-fields should be unsigned for predictable behavior.
+
+Avoid using bit-fields with sizeof().
+
+Compiler-dependent behavior.
+
