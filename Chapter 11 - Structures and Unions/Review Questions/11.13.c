3e330f6e590aec2044807820cbd7f40f95eb9d05
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-24 09:07:25 +0600

    Add files via upload

---
+++ Chapter 11 - Structures and Unions/Review Questions/11.13.c
@@ -0,0 +1,7 @@
+
+1.13 State the rules for initializing structures
+Must initialize in the order of declaration.
+
+Can use designated initializers.
+
+Uninitialized members get default values (0 for int, NULL for pointers).
