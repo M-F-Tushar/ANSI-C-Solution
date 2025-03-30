1ce05c6985865ad04c0d2fd19175a5cf4f8031ad
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-30 17:02:17 +0600

    Add files via upload

---
+++ Chapter 9 - Character Arrays and Strings/Review Questions/9.7.c
@@ -0,0 +1,15 @@
+9.7 Which of the following statements will correctly store the concatenation of strings s1 and s2 in string s3?
+Options:
+✅ (b) strcat(s1, s2, s3);
+
+strcat(s3, s1) → ✅ Valid
+
+strcat(s3, s2) → ✅ Valid
+
+✅ Correct Answer:
+c
+Copy
+Edit
+strcpy(s3, s1);
+strcat(s3, s2);
+
