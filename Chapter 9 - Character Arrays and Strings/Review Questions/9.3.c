da322e601ecb4d19c90bd8371fcdb43844be6ee0
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-30 17:01:13 +0600

    Add files via upload

---
+++ Chapter 9 - Character Arrays and Strings/Review Questions/9.3.c
@@ -0,0 +1,10 @@
+9.3 Describe the limitations of using getchar and scanf functions for reading strings.
+✅ Answer:
+
+getchar() reads a single character at a time.
+
+scanf() stops reading at the first whitespace (space, tab, newline).
+
+scanf() can cause buffer overflow if the input exceeds the buffer size.
+
+
