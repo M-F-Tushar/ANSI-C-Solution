0712a17c545de1222346511c194f815aa7ff8a3f
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-30 17:02:49 +0600

    Add files via upload

---
+++ Chapter 9 - Character Arrays and Strings/Review Questions/9.8.c
@@ -0,0 +1,8 @@
+9.8 What will be the output of the following statement?
+
+printf("%d", strcmp("push", "pull"));
+✅ Answer:
+
+"push" < "pull" (since 's' > 'l' in ASCII).
+
+Therefore, strcmp() returns a positive value → 1
