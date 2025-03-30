c34bd986a767eca5f1cce65f5e514f56b6d08914
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-30 17:01:12 +0600

    Add files via upload

---
+++ Chapter 9 - Character Arrays and Strings/Review Questions/9.2.c
@@ -0,0 +1,16 @@
+9.2 Fill in the blanks:
+(a) We can use the conversion specification _________ in scanf to read a line of text.
+✅ Answer: %[^\n]
+
+(b) The function _________ does not require any specification to read a string from the keyboard.
+✅ Answer: gets()
+
+(c) The printf may be replaced by _________ function for printing strings.
+✅ Answer: puts()
+
+(d) The function strncat has _________ parameters.
+✅ Answer: 3
+
+strncat(dest, src, n)
+
+
