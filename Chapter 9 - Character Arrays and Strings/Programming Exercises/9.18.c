f4549264deb77e8cfc61ed3c9f2bd367fc9ba77e
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-31 13:04:25 +0600

    Add files via upload

---
+++ Chapter 9 - Character Arrays and Strings/Programming Exercises/9.18.c
@@ -0,0 +1,29 @@
+9.18 - Replace Spaces with Double Spaces
+Solution in C:
+c
+Copy
+Edit
+#include <stdio.h>
+
+int main() {
+    char str[200], newStr[400];
+    int i = 0, j = 0;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    while (str[i] != '\0') {
+        if (str[i] == ' ') {
+            newStr[j++] = ' ';
+            newStr[j++] = ' ';
+        } else {
+            newStr[j++] = str[i];
+        }
+        i++;
+    }
+    newStr[j] = '\0';
+
+    printf("Modified String: %s\n", newStr);
+
+    return 0;
+}
