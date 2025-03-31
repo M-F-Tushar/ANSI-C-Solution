4e10f9612bc06f11619e4b53ab8ef3efb318f5e9
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-31 13:03:51 +0600

    Add files via upload

---
+++ Chapter 9 - Character Arrays and Strings/Programming Exercises/9.15.c
@@ -0,0 +1,42 @@
+9.15 - Displaying Number Pattern from String
+Problem:
+Given the string
+
+c
+Copy
+Edit
+char str[] = "123456789";
+Write a program to display the following pattern:
+
+Copy
+Edit
+1
+232
+34543
+4567654
+567898765
+Solution in C:
+c
+Copy
+Edit
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[] = "123456789";
+    int len = strlen(str);
+
+    for (int i = 0; i < len / 2 + 1; i++) {
+        // Print increasing part
+        for (int j = i; j < len / 2 + i + 1; j++) {
+            printf("%c", str[j]);
+        }
+        // Print decreasing part
+        for (int j = len / 2 + i - 1; j >= i; j--) {
+            printf("%c", str[j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
