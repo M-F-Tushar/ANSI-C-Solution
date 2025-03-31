981103f5fdc5e37d831f64bfaad54cdbb2b85f87
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-31 06:51:37 +0600

    Delete Chapter 9 - Character Arrays and Strings/9.10.c

--- Chapter 9 - Character Arrays and Strings/9.10.c
+++
@@ -1,9 +0,0 @@
-9.10 What will be the output of the following segment?
-
-char s1[] = "Kolkata";
-char s2[] = "Pune";
-strcpy(s1, s2);
-printf("%s", s1);
-✅ Answer: Pune
-
-strcpy(s1, s2) copies "Pune" into s1.
