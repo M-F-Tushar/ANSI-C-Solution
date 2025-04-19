b2a96000116550f4b3ea81b9205a5d150d83d258
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-19 06:24:31 +0600

    Add files via upload

---
+++ Chapter 12 - Pointer/Review Questions/12.16.c
@@ -0,0 +1,9 @@
+✅ 12.16 Which is the correct way of declaring a pointer to a function? Why?
+(a) int (*p)(void); ✅
+(b) int *p(void); ❌
+
+Explanation:
+
+(a) Correct: It declares p as a pointer to a function that takes void and returns int.
+
+(b) Incorrect: Declares a function that returns a pointer to int.
