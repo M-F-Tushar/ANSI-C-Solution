9e76f60b1ad0e9ed71967f23ecbe0458799ec21f
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-19 06:21:27 +0600

    Add files via upload

---
+++ Chapter 12 - Pointer/Review Questions/12.10.c
@@ -0,0 +1,9 @@
+✅ 12.10 Difference between:
+
+(a) name + 10
+(b) *(name + 10)
+Answer:
+
+(a) gives the address of the 11th character in the array.
+
+(b) gives the value stored at that address (i.e., the actual character at index 10).
