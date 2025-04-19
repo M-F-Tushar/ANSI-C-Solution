07c4a6bf83f23e538c3f905211eacf007de0abc3
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-19 06:17:25 +0600

    Add files via upload

---
+++ Chapter 12 - Pointer/Review Questions/12.3.c
@@ -0,0 +1,10 @@
+
+✅ 12.3 What is a pointer? How can it be initialized?
+Answer:
+A pointer is a variable that stores the memory address of another variable. It is declared using the * operator.
+Initialization Example:
+
+int a = 5;
+int *ptr = &a;
+Here, ptr is a pointer to an integer and is initialized with the address of a.
+
