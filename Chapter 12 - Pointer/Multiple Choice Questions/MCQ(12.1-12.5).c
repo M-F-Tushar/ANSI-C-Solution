f5ed2ad42c52db0153c9c7273d21646db1adb491
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-19 06:16:33 +0600

    Add files via upload

---
+++ Chapter 12 - Pointer/Multiple Choice Questions/MCQ(12.1-12.5).c
@@ -0,0 +1,21 @@
+12.1 A pointer in C language is:
+Answer: (d) All of the above
+Reason: A pointer stores the address of a location, is useful in linked lists, and can access array elements.
+
+12.2 Which constant values can be assigned to a pointer variable?
+Answer: (d) Both 1 and 2 (i.e., 0 and NULL)
+Reason: 0 and NULL both represent a null pointer constant.
+
+12.3 Which of the following operations is allowed on a pointer variable?
+Answer: (d) All of these
+Reason: Pointer arithmetic like ptr+1, ptr-1, ptr++, ptr-- is valid in C.
+
+12.4 Which of the following is a valid indirection notation for a pointer?
+*Answer: (a) ptr
+Reason: *ptr is used to dereference a pointer and access the value at the address.
+
+12.5 Which of the following is incorrect about pointers?
+Answer: (a) A pointer variable stores NULL value until it is initialized.
+Reason: This is incorrect. A pointer contains garbage until explicitly initialized.
+
+
