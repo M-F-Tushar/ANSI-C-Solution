41a1a7fc07fe443a1266b5b139cf68d6fea11cc5
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-19 06:34:28 +0600

    Add files via upload

---
+++ Chapter 12 - Pointer/Interview Questions/12.2.c
@@ -0,0 +1,11 @@
+
+✅ 12.2 What are the types of pointers?
+Null Pointer: points to nothing (int *ptr = NULL;)
+
+Void Pointer: generic pointer (void *ptr;)
+
+Function Pointer: points to a function
+
+Dangling Pointer: points to freed memory
+
+Wild Pointer: uninitialized pointer
