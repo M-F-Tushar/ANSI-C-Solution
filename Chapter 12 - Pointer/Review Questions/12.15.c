a1e7463b96e28cffbc6898f7f31a66d361b5c6b7
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-19 06:24:29 +0600

    Add files via upload

---
+++ Chapter 12 - Pointer/Review Questions/12.15.c
@@ -0,0 +1,13 @@
+✅ 12.15 Do the declarations
+
+char s[5];
+char *s;
+Represent the same? Explain.
+
+Answer: No, they do not represent the same.
+
+char s[5]; declares a character array of size 5. Memory is allocated for 5 characters.
+
+char *s; declares a pointer to a character, which can point to any memory location storing characters.
+
+So, s[5] has allocated memory while *s only points to some location.
