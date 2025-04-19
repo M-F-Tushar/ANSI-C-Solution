9003aea0a18ae043d3e9618f55c9c02c25729457
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-19 06:23:30 +0600

    Add files via upload

---
+++ Chapter 12 - Pointer/Review Questions/12.14.c
@@ -0,0 +1,8 @@
+✅ 12.14 Are these function definitions equivalent?
+
+a) void search(int* m[], int x)
+b) void search(int** m, int x)
+Answer: Yes, they are equivalent.
+
+Explanation:
+In C, an array of pointers (int* m[]) is treated the same as a pointer to a pointer (int** m). Both can be used to pass a 2D array (or array of pointers) to a function.
