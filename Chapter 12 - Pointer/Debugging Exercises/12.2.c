8b32622c3abd0005e6cf659d0c1f88999e4b95fc
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-19 06:26:53 +0600

    Add files via upload

---
+++ Chapter 12 - Pointer/Debugging Exercises/12.2.c
@@ -0,0 +1,5 @@
+✅ 12.2 Find errors
+(a) int x = 10; ✅
+(b) int *y = 10; ❌ Error: 10 is not an address. Should be int *y = &x;
+(c) int a, *b = &a; ✅
+(d) int m; int 29x = &m; ❌ Invalid variable name (29x starts with digit)
