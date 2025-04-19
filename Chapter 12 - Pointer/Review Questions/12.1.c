58092b89fefa602430f197e7987493559700acf1
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-19 06:16:51 +0600

    Add files via upload

---
+++ Chapter 12 - Pointer/Review Questions/12.1.c
@@ -0,0 +1,13 @@
+12.1 State whether the following statements are true or false:
+
+Statement	                                                            Answer
+(a) Pointer constants are the addresses of memory locations.	        True
+(b) The underlying type of a pointer variable is void.	                False
+(c) Pointer variables are declared using the address operator.	        False (They are declared using *, address operator & is used during assignment)
+(d) It is possible to cast a pointer to float as a pointer to integer.	True
+(e) Pointers to pointers is a term used to describe pointers whose contents are the address of another pointer.	True
+(f) A pointer can never be subtracted from another pointer.	            False (They can be subtracted if pointing to the same array)
+(g) An integer can be added to a pointer.	                            True
+(h) Pointers cannot be used as formal parameters in headers to function definitions.	False
+(i) When an array is passed as an argument to a function, a pointer is passed.	True
+(j) Value of a local variable in a function can be changed by another function.	True, if passed by reference using pointers.
