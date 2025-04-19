232da0e4f2efe23673a6c501744b01b04fffd166
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-19 06:20:21 +0600

    Add files via upload

---
+++ Chapter 12 - Pointer/Review Questions/12.7.c
@@ -0,0 +1,13 @@
+
+✅ 12.7 Describe typical applications of pointers in developing programs.
+Answer:
+
+Dynamic memory allocation
+
+Efficient array and string handling
+
+Function arguments via reference (call by reference)
+
+Implementing data structures (linked lists, trees)
+
+Handling buffers and hardware interaction
