6b9d5d37ba957d28731d5496e825733b7bb01754
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-19 06:17:02 +0600

    Add files via upload

---
+++ Chapter 12 - Pointer/Review Questions/12.2.c
@@ -0,0 +1,16 @@
+12.2 Fill in the blanks:
+
+(a) A pointer variable contains as its value the address of another variable.
+
+(b) The address operator returns the value of the address to which its operand points.
+
+(c) The indirection operator is used with a pointer to de-reference the address contained in the pointer.
+
+(d) The pointer that is declared as void cannot be de-referenced.
+
+(e) The only integer that can be assigned to a pointer variable is 0.
+
+(f) The process of calling a function using pointers to pass the addresses of variables is known as call by reference.
+
+(g) Void pointer is called generic pointer and can be used to represent any pointer type.
+
