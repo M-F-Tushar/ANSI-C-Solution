a5b2fad52968391ad35bf1b6889f295bde69a6d2
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-19 06:21:00 +0600

    Add files via upload

---
+++ Chapter 12 - Pointer/Review Questions/12.8.c
@@ -0,0 +1,14 @@
+✅ 12.8 What arithmetic operators are permitted on pointers?
+Answer:
+
+Addition (ptr + n)
+
+Subtraction (ptr - n)
+
+Increment (ptr++)
+
+Decrement (ptr--)
+
+Difference between two pointers (ptr1 - ptr2)
+
+Multiplication and division are not permitted.
