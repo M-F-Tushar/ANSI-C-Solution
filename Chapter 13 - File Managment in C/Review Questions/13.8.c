3a408a97c14a4a9e35da82e1e2304f2c18a8a162
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-20 07:06:41 +0600

    Add files via upload

---
+++ Chapter 13 - File Managment in C/Review Questions/13.8.c
@@ -0,0 +1,6 @@
+✅ 13.8 What are the common uses of rewind() and ftell() functions?
+rewind(fp) – Moves the file pointer to the beginning of the file.
+
+ftell(fp) – Returns the current position of the file pointer (as a long int).
+
+
