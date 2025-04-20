a8ebe6b14cf3b7792e68d55e6e2bada2e5621b48
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-20 07:07:36 +0600

    Add files via upload

---
+++ Chapter 13 - File Managment in C/Review Questions/13.12.c
@@ -0,0 +1,12 @@
+✅ 13.12 What does this statement do?
+c
+Copy code
+while ((c = getchar()) != EOF)
+    putc(c, fl);
+Reads characters from input (keyboard) using getchar()
+
+Writes them to a file (fl) using putc()
+
+Stops at EOF (Ctrl+Z in Windows, Ctrl+D in Linux)
+
+
