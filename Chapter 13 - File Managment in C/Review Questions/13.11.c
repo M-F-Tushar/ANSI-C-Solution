058322239de9b07278eb9e24fe5f3f37568304be
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-20 07:07:34 +0600

    Add files via upload

---
+++ Chapter 13 - File Managment in C/Review Questions/13.11.c
@@ -0,0 +1,12 @@
+✅ 13.11 What does FILE (*p)(void); mean?
+⚠️ This is a tricky one!
+
+It declares a function pointer named p that:
+
+Takes no arguments
+
+Returns a FILE
+
+It’s not a typical or practical C usage — possibly a confusing or incorrect declaration.
+
+
