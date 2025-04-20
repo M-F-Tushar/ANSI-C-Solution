07a4d6c273ab48dadf388de3ba124c66dbf02b2a
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-20 07:06:39 +0600

    Add files via upload

---
+++ Chapter 13 - File Managment in C/Review Questions/13.7.c
@@ -0,0 +1,8 @@
+✅ 13.7 How does append mode differ from write mode?
+Write mode ("w"):
+
+Creates a new file or deletes the contents of an existing one.
+
+Append mode ("a"):
+
+Creates a file if it doesn’t exist, adds new data at the end if it does.
