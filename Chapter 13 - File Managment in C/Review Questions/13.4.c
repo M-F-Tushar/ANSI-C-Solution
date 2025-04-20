2a9dda8c650648f644a98fd8d3385d7535c06956
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-20 07:05:18 +0600

    Add files via upload

---
+++ Chapter 13 - File Managment in C/Review Questions/13.4.c
@@ -0,0 +1,4 @@
+✅ 13.4 What is the significance of EOF?
+EOF (End Of File) is a special constant (usually -1) that signals the end of a file stream.
+
+It’s returned by functions like getc() or scanf() when there is no more data to read.
