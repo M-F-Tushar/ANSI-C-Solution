4006b30dc00b16067e1461072a541eb4449a0ec3
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-20 07:08:00 +0600

    Add files via upload

---
+++ Chapter 13 - File Managment in C/Review Questions/13.13.c
@@ -0,0 +1,10 @@
+✅ 13.13 What does this statement do?
+
+
+while ((m = getw(fl)) != EOF)
+    printf("%5d", m);
+Reads integers from file fl using getw()
+
+Prints them right-aligned in 5 spaces
+
+Stops when EOF is encountered
