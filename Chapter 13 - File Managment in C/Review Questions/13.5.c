41e2b99e67a8e63aa5ffc42302673c5012bd6365
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-20 07:05:43 +0600

    Add files via upload

---
+++ Chapter 13 - File Managment in C/Review Questions/13.5.c
@@ -0,0 +1,7 @@
+
+✅ 13.5 Why is it necessary to close a file during execution if files are auto-closed after program ends?
+To free up system resources (memory, file handles).
+
+To flush the buffer (ensure all data is written).
+
+Prevent data corruption or data loss in case of crashes.
