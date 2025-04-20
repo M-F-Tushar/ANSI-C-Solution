16c80d5d7d8505be57a430e3a378f3f5aec150e5
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-20 07:08:53 +0600

    Add files via upload

---
+++ Chapter 13 - File Managment in C/Review Questions/13.15.c
@@ -0,0 +1,3 @@
+
+✅ 13.15 Purpose of the following functions:
+(a) feof() – Checks if end-of-file has been reached for a file. (b) ferror() – Checks if a file error occurred (e.g., disk issue, write failure).
