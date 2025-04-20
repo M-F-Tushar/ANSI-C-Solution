f6f01527c04d8464db7c0bd0a89959b88cec49a5
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-20 07:05:44 +0600

    Add files via upload

---
+++ Chapter 13 - File Managment in C/Review Questions/13.6.c
@@ -0,0 +1,18 @@
+✅ 13.6 Distinguish between the following:
+(a) getc vs getchar:
+
+getc(fp) reads from a file.
+
+getchar() reads from standard input (stdin), equivalent to getc(stdin).
+
+(b) printf vs fprintf:
+
+printf() outputs to console.
+
+fprintf(FILE *fp, ...) outputs to a file.
+
+(c) feof vs ferror:
+
+feof(fp) checks if end of file has been reached.
+
+ferror(fp) checks if an I/O error has occurred during operations.
