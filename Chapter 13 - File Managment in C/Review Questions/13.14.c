7eb7ad1f0a5fbbfd7ab16374daec546264b4939b
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-20 07:08:02 +0600

    Add files via upload

---
+++ Chapter 13 - File Managment in C/Review Questions/13.14.c
@@ -0,0 +1,10 @@
+✅ 13.14 What does this segment do?
+
+for (i = 1; i <= 5; i++)
+{
+    fscanf(stdin, "%s", name);
+    fprintf(fp, "%s", name);
+}
+Takes 5 strings from user input (standard input).
+
+Writes them (without newline!) to file fp.
